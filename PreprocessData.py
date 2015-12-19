df71cf032ba4865fce53fc82517c759485d4e3b2
JodesL <jonah.lam@hotmail.com>
2015-12-19 14:20:50 -0500

    Pre-process v1
    -created aggregate_data
    -normalized data

--- PreprocessData.py
+++ PreprocessData.py
@@ -87,80 +87,105 @@ for game in inputs_raw:
 aggregate_team = [[0 for i in range(15)] for j in range(30)]
 data = []
 
-#
-# home_team_index = []
-# away_team_index = []
-# time_index = []
-#
-# for match in training_games:
-#     home_team_index.append(match[0:2])
-#     away_team_index.append(match[2:0:-1])
-
-
-print(training_games)
-
-print(inputs_raw)
-print(team_index)
-
 for game in training_games:
+    data.append([])
     for row in inputs_raw:
         if game[1] == row[1]:
-            data.append([])
             if game[0] == row[0]:
                 for i in range(len(aggregate_team[0])):
                     aggregate_team[team_index.index(row[0])][i] += int(row[i+2])
-                data.append(aggregate_team[team_index.index(row[0])])
+                data[-1][0:0] = aggregate_team[team_index.index(row[0])]
 
             if game[2] == row[0]:
-                 for i in range(len(aggregate_team[0])):
+                for i in range(len(aggregate_team[0])):
                     aggregate_team[team_index.index(row[0])][i] += int(row[i+2])
 
-                 data.append(aggregate_team[team_index.index(row[0])])
+                data[-1].extend(aggregate_team[team_index.index(row[0])])
+    data[-1][0:0] = [int(team_legend[team_index.index(game[2])][i]) for i in range(2, len(team_legend[0]))]
+    data[-1][0:0] = [int(team_legend[team_index.index(game[0])][i]) for i in range(2, len(team_legend[0]))]
+    data[-1].insert(0, int(game[3]))
 
-# inputs_raw_index = []
-#
-# for game in inputs_raw:
-#     inputs_raw_index.append(game[0:2])
-#
-# import_matrix = []
-#
-# for i in range(0, 2):
-#     import_matrix.append(inputs_raw[inputs_raw_index.index(home_team_index[i])])
-#
-#
-# print(home_team_index)
-# print(away_team_index)
-# print(training_games)
-# print(inputs_raw)
-# print(inputs_raw_index)
-#
-# print(import_matrix)
 
+#data structure:
+    # 0 - win indicator
+    # 1:30 - team 1
+    # 31:60 - team 2
+    # 61 - GP (team1)
+    # 62 - Wins
+    # 63 - Losses
+    # 64 - Overtime Losses
+    # 65 - Points
+    # 66 - Goals For
+    # 67 - Goals Against
+    # 68 - Shots For
+    # 69 - Shots Againsts
+    # 70 - PPG
+    # 71 - PP opp
+    # 72 - TS
+    # 73 - PPGA
+    # 74 - FOW
+    # 75 - FOL
+    # 76 - GP (team2)
+    # 77 - Wins
+    # 78 - Losses
+    # 79 - Overtime Losses
+    # 80 - Points
+    # 81 - Goals For
+    # 82 - Goals Against
+    # 83 - Shots For
+    # 84 - Shots Againsts
+    # 85 - PPG
+    # 86 - PP opp
+    # 87 - TS
+    # 88 - PPGA
+    # 89 - FOW
+    # 90 - FOL
+    # 91 - PP% (team1)
+    # 92 - PK% (team1)
+    # 93 - FOW% (team1)
+    # 94 - PP% (team2)
+    # 95 - PK% (team2)
+    # 96 - FOW% (team2)
+
+
+
+#adding percentage variables
+for agg_game in data:
+
+    #home
+    agg_game.append(agg_game[70]/agg_game[71])
+    agg_game.append((agg_game[72]-agg_game[73])/agg_game[72])
+    agg_game.append(agg_game[74]/(agg_game[74]+agg_game[75]))
+
+    #away
+    agg_game.append(agg_game[85]/agg_game[86])
+    agg_game.append((agg_game[87]-agg_game[88])/agg_game[87])
+    agg_game.append(agg_game[89]/(agg_game[89]+agg_game[90]))
+
+print(data)
+
+teams_inputs = np.array(data)[:, 0:61]  # These should not be normalized (team numbers)
+stats_inputs = np.array(data)[:, 61:]
 
 # for i in range(4):
-#   print(inputs_raw[i])
+#     print(teams_inputs[i])
+#     print(stats_inputs[i])
+
+normalized_inputs = (stats_inputs - np.mean(stats_inputs, 0)) / np.std(stats_inputs, 0, ddof=1)
+
+# print(np.mean(normalized_inputs, 0))
+# print(np.std(normalized_inputs, 0, ddof=1))
 #
-# teams_inputs = np.array(inputs)[:, 0:2]  # These should not be normalized (team numbers)
-# stats_inputs = np.array(inputs)[:, 2:]
-#
-# # for i in range(4):
-# #     print(teams_inputs[i])
-# #     print(stats_inputs[i])
-#
-# normalized_inputs = (stats_inputs - np.mean(stats_inputs, 0)) / np.std(stats_inputs, 0, ddof=1)
-#
-# # print(np.mean(normalized_inputs, 0))
-# # print(np.std(normalized_inputs, 0, ddof=1))
-# # for i in range(4):
-# #     print(normalized_inputs[i])
-#
-# final_inputs = np.concatenate((teams_inputs, normalized_inputs), 1)
-#
-# # for i in range(4):
-# #     print(final_inputs[i])
-#
-# file = open('InputData2014-15_Final.csv','w')
-# writer = csv.writer(file, lineterminator='\n')
-# for row in final_inputs:
-#     writer.writerow(["{:.15f}".format(x) for x in row])  # Need to preserve as many decimals as possible
-# file.close()
+# for i in range(4):
+#     print(normalized_inputs[i])
+
+final_inputs = np.concatenate((teams_inputs, normalized_inputs), 1)
+
+# for i in range(4):
+#     print(final_inputs[i])
+
+file = open('InputData2014-15_Final.csv', 'w')
+writer = csv.writer(file, lineterminator='\n')
+for row in final_inputs:
+    writer.writerow(["{:.15f}".format(x) for x in row])  # Need to preserve as many decimals as possible
+file.close()
